7ad8183b1fc819c2e10bb483913d7b8776c1fa81
vfx-beavers <63024074+vfx-beavers@users.noreply.github.com>
2023-12-07 04:52:09 +0300

    v2

---
+++ solution/service_dds/src/dds_loader/repository/dds_builder.py
@@ -0,0 +1,185 @@
+import uuid
+from datetime import datetime
+from typing import Any, Dict, List
+from .objects import *
+
+class OrderDdsBuilder:
+    def init(self, dict: Dict) -> None:
+        self._dict = dict
+        self.source_system = "stg-service-orders"
+        self.order_ns_uuid = uuid.UUID('7f288a2e-0ad0-4039-8e59-6c9838d87307')
+    
+    def _uuid(self, obj: Any) -> uuid.UUID:
+        return uuid.uuid5(namespace=self.order_ns_uuid, name=str(obj))
+    
+    # hubs
+    def h_user(self) -> H_User:
+        user_id = str(self._dict['user']['id'])
+        return H_User(
+            h_user_pk=self._uuid(user_id),
+            user_id=user_id,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system
+        )
+    
+    def h_product(self) -> List[H_Product]:
+        products = []
+        for prod_dict in self._dict['products']:
+            prod_id = prod_dict['id']
+            products.append(
+                H_Product(
+                    h_product_pk=self._uuid(prod_id),
+                    product_id=prod_id,
+                    load_dt=datetime.utcnow(),
+                    load_src=self.source_system
+                )
+            )
+        return products
+    
+    def h_category(self) -> List[H_Category]:
+        categories = []
+        for cat_dict in self._dict['products']:
+            cat_id = cat_dict['id']
+            cat_name = cat_dict['category']
+            categories.append(
+                H_Category(
+                    h_category_pk=self._uuid(cat_id),
+                    category_name=cat_name,
+                    load_dt=datetime.utcnow(),
+                    load_src=self.source_system
+                )
+            )
+        return categories
+
+    def h_order(self) -> H_Order:
+        order_id = str(self._dict['id'])
+        order_dt = self._dict['date']
+        return H_Order(
+            h_order_pk=self._uuid(order_id),
+            order_id=order_id,
+            order_dt=order_dt,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system
+        )
+    
+    def h_restaurant(self) -> H_Restaurant:
+        restaurant_id = str(self._dict['restaurant']['id'])
+        return H_Restaurant(
+            h_restaurant_pk=self._uuid(restaurant_id),
+            restaurant_id=restaurant_id,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system
+        )
+    
+    # links
+    def l_order_user(self, h_user_pk: uuid.UUID, h_order_pk: uuid.UUID) -> L_order_user:
+        return L_order_user(
+            hk_order_user_pk=self._uuid(h_order_pk),
+            h_user_pk=h_user_pk,
+            h_order_pk=h_order_pk,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system
+        )
+    
+    def l_order_product(self, h_product_pk_list: List[uuid.UUID], h_order_pk: uuid.UUID) -> List[L_order_product]:
+        order_product = []
+        for h_product_pk in h_product_pk_list:
+            order_product.append(
+                L_order_product(
+                    hk_order_product_pk=self._uuid(h_order_pk),
+                    h_product_pk=h_product_pk,
+                    h_order_pk=h_order_pk,
+                    load_dt=datetime.utcnow(),
+                    load_src=self.source_system
+                )
+            )
+        return order_product
+    
+    def l_product_category(self, h_product_pk_list: List[uuid.UUID], h_category_pk_list: List[uuid.UUID]) -> List[L_product_category]:
+        product_category = []
+        for h_product_pk in h_product_pk_list:
+          for h_category_pk in h_category_pk_list:
+              product_category.append(
+                  L_product_category(
+                      hk_product_category_pk=self._uuid(h_product_pk),
+                      h_product_pk=h_product_pk,
+                      h_category_pk=h_category_pk,
+                      load_dt=datetime.utcnow(),
+                      load_src=self.source_system
+                  )
+              )
+        return product_category
+
+    def l_product_restaurant(self, h_restaurant_pk: uuid.UUID, h_product_pk_list: List[uuid.UUID]) -> List[L_product_restaurant]:
+        product_restaurant = []
+        for h_product_pk in h_product_pk_list:
+            product_restaurant.append(
+                L_product_restaurant(
+                    hk_product_restaurant_pk=self._uuid(h_product_pk),
+                    h_restaurant_pk=h_restaurant_pk,
+                    h_product_pk=h_product_pk,
+                    load_dt=datetime.utcnow(),
+                    load_src=self.source_system
+                )
+            )
+        return product_restaurant
+    
+    # sats
+    def s_order_cost(self, h_order_pk: uuid.UUID) -> S_order_cost:
+        cost = self._dict['cost']
+        payment = self._dict['payment']
+        return S_order_cost(
+            h_order_pk=h_order_pk,
+            cost=cost,
+            payment=payment,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system,
+            hk_order_cost_hashdiff=self._uuid(h_order_pk)
+        )
+    
+    def s_order_status(self, h_order_pk: uuid.UUID) -> S_order_status:
+        status = self._dict['status']
+        return S_order_status(
+            h_order_pk=h_order_pk,
+            status=status,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system,
+            hk_order_status_hashdiff=self._uuid(h_order_pk)
+        )
+    
+    def s_product_names(self, h_product_pk_list: List[uuid.UUID]) -> List[S_product_names]:
+        product_names = []
+        for product in self._dict["products"]:
+            if self._uuid(product["id"]) in h_product_pk_list:
+                product_names.append(
+                    S_product_names(
+                        h_product_pk=self._uuid(product["id"]),
+                        name=product["name"],
+                        load_dt=datetime.utcnow(),
+                        load_src=self.source_system,
+                        hk_product_names_hashdiff=self._uuid(product["id"])
+                    )
+                )
+        return product_names
+
+    def s_restaurant_names(self, h_restaurant_pk: uuid.UUID) -> S_restaurant_names:
+        name = self._dict['restaurant']['name']
+        return S_restaurant_names(
+            h_restaurant_pk=h_restaurant_pk,
+            name=name,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system,
+            hk_restaurant_names_hashdiff=self._uuid(h_restaurant_pk)
+        )
+    
+    def s_user_names(self, h_user_pk: uuid.UUID) -> S_user_names:
+        username = self._dict['user']['name']
+        userlogin = self._dict['user']['login']
+        return S_user_names(
+            h_user_pk=h_user_pk,
+            username=username,
+            userlogin=userlogin,
+            load_dt=datetime.utcnow(),
+            load_src=self.source_system,
+            hk_user_names_hashdiff=self._uuid(h_user_pk)
+        )
--- solution/service_dds/src/dds_loader/repository/dds_repository.py
+++ solution/service_dds/src/dds_loader/repository/dds_repository.py
@@ -2,13 +2,14 @@ from datetime import datetime
 from typing import Any, Dict, List
 from lib.pg import PgConnect
 
+from .objects import *
 
 class DdsRepository:
     def __init__(self, db: PgConnect) -> None:
         self._db = db
 
     #хабы
-    def h_user_insert(self, h_user_pk: str, user_id: str, load_dt: datetime, load_src: str) -> None:
+    def h_user_insert(self, user: H_User) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -18,48 +19,50 @@ class DdsRepository:
                     on conflict(user_id) do nothing;
                     """,
                     {
-                    'h_user_pk': h_user_pk,
-                    'user_id': user_id,
-                    'load_dt': load_dt,
-                    'load_src': load_src
+                        'h_user_pk': user.h_user_pk,
+                        'user_id': user.user_id,
+                        'load_dt': user.load_dt,
+                        'load_src': user.load_src
                     }
                 )
-
-    def h_product_insert(self, h_product_pk: str, product_id: str, load_dt: datetime, load_src: str) -> None:
+                
+    def h_product_insert(self, objs: List[H_Product]) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(
+                for obj in objs:
+                    cur.execute(
                     """
                     insert into dds.h_product (h_product_pk, product_id, load_dt, load_src)
                     values (%(h_product_pk)s, %(product_id)s, %(load_dt)s, %(load_src)s)
                     on conflict(product_id) do nothing;
                     """,
                     {
-                    'h_product_pk': h_product_pk,
-                    'product_id': product_id,
-                    'load_dt': load_dt,
-                    'load_src': load_src
-                    }
-                )
-
-    def h_category_insert(self, h_category_pk: str, category_name: str, load_dt: datetime, load_src: str) -> None:
+                            'h_product_pk': obj.h_product_pk,
+                            'product_id': obj.product_id,
+                            'load_dt': obj.load_dt,
+                            'load_src': obj.load_src
+                        }
+                    ) 
+    
+    def h_category_insert(self, objs: List[H_Category]) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(
+                for obj in objs:
+                    cur.execute(
                     """
                     insert into dds.h_category (h_category_pk, category_name, load_dt, load_src)
                     values (%(h_category_pk)s, %(category_name)s, %(load_dt)s, %(load_src)s)
                     on conflict(category_name) do nothing;
                     """,
                     {
-                    'h_category_pk': h_category_pk,
-                    'category_name': category_name,
-                    'load_dt': load_dt,
-                    'load_src': load_src
-                    }
-                )
+                            'h_category_pk': obj.h_category_pk,
+                            'category_name': obj.category_name,
+                            'load_dt': obj.load_dt,
+                            'load_src': obj.load_src
+                        }
+                    )
 
-    def h_restaurant_insert(self, h_restaurant_pk: str, restaurant_id: str, load_dt: datetime, load_src: str) -> None:
+    def h_restaurant_insert(self, restaurant: H_Restaurant) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -69,14 +72,14 @@ class DdsRepository:
                     on conflict(restaurant_id) do nothing;
                     """,
                     {
-                    'h_restaurant_pk': h_restaurant_pk,
-                    'restaurant_id': restaurant_id,
-                    'load_dt': load_dt,
-                    'load_src': load_src
+                        'h_restaurant_pk': restaurant.h_restaurant_pk,
+                        'restaurant_id': restaurant.restaurant_id,
+                        'load_dt': restaurant.load_dt,
+                        'load_src': restaurant.load_src
                     }
                 )
 
-    def h_order_insert(self, h_order_pk: str, order_id: str, order_dt:datetime, load_dt: datetime, load_src: str) -> None:
+    def h_order_insert(self, order: H_Order) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -86,70 +89,73 @@ class DdsRepository:
                     on conflict(order_id) do nothing;
                     """,
                     {
-                    'h_order_pk': h_order_pk,
-                    'order_id': order_id,
-                    'load_dt': load_dt,
-                    'order_dt': order_dt,
-                    'load_src': load_src
+                        'h_order_pk': order.h_order_pk,
+                        'order_id': order.order_id,
+                        'order_dt': order.order_dt,
+                        'load_dt': order.load_dt,
+                        'load_src': order.load_src
                     }
                 )
 
     #линки
-    def l_order_product_insert(self, hk_order_product_pk:str, h_order_pk: str, h_product_pk: str, load_dt:datetime, load_src: str) -> None:
+    def l_order_product_insert(self, objs: List[L_order_product]) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(
+                for obj in objs:
+                    cur.execute(
                     """
                     insert into dds.l_order_product (hk_order_product_pk, h_order_pk, h_product_pk, load_dt, load_src)
                     values (%(hk_order_product_pk)s, %(h_order_pk)s, %(h_product_pk)s, %(load_dt)s, %(load_src)s)
                     on conflict(hk_order_product_pk) do nothing;
                     """,
                     {
-                    'hk_order_product_pk': hk_order_product_pk,
-                    'h_order_pk': h_order_pk,
-                    'h_product_pk': h_product_pk,
-                    'load_dt': load_dt,
-                    'load_src': load_src
-                    }
-                )
+                            'hk_order_product_pk': obj.hk_order_product_pk,
+                            'h_product_pk': obj.h_product_pk,
+                            'h_order_pk': obj.h_order_pk,
+                            'load_dt': obj.load_dt,
+                            'load_src': obj.load_src
+                        }
+                    )
 
-    def l_product_restaurant_insert(self, hk_product_restaurant_pk:str, h_product_pk: str, h_restaurant_pk: str, load_dt:datetime, load_src: str) -> None:
+    def l_product_restaurant_insert(self, objs: List[L_product_restaurant]) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(
+                for obj in objs:
+                    cur.execute(
                     """
                     insert into dds.l_product_restaurant (hk_product_restaurant_pk, h_product_pk, h_restaurant_pk, load_dt, load_src)
                     values (%(hk_product_restaurant_pk)s, %(h_product_pk)s, %(h_restaurant_pk)s, %(load_dt)s, %(load_src)s)
                     on conflict(hk_product_restaurant_pk) do nothing;
                     """,
                     {
-                    'hk_product_restaurant_pk': hk_product_restaurant_pk,
-                    'h_product_pk': h_product_pk,
-                    'h_restaurant_pk': h_restaurant_pk,
-                    'load_dt': load_dt,
-                    'load_src': load_src
-                    }
-                )
+                            'hk_product_restaurant_pk': obj.hk_product_restaurant_pk,
+                            'h_restaurant_pk': obj.h_restaurant_pk,
+                            'h_product_pk': obj.h_product_pk,
+                            'load_dt': obj.load_dt,
+                            'load_src': obj.load_src
+                        }
+                    )
 
-    def l_product_category_insert(self, hk_product_category_pk:str, h_product_pk: str, h_category_pk: str, load_dt:datetime, load_src: str) -> None:
+    def l_product_category_insert(self, objs: List[L_product_category]) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(
+                for obj in objs:
+                    cur.execute(
                     """
                     insert into dds.l_product_category (hk_product_category_pk, h_product_pk, h_category_pk, load_dt, load_src)
                     values (%(hk_product_category_pk)s, %(h_product_pk)s, %(h_category_pk)s, %(load_dt)s, %(load_src)s)
                     on conflict(hk_product_category_pk) do nothing;
                     """,
                     {
-                    'hk_product_category_pk': hk_product_category_pk,
-                    'h_product_pk': h_product_pk,
-                    'h_category_pk': h_category_pk,
-                    'load_dt': load_dt,
-                    'load_src': load_src
-                    }
-                )
+                            'hk_product_category_pk': obj.hk_product_category_pk,
+                            'h_product_pk': obj.h_product_pk,
+                            'h_category_pk': obj.h_category_pk,
+                            'load_dt': obj.load_dt,
+                            'load_src': obj.load_src
+                        }
+                    )
 
-    def l_order_user_insert(self, hk_order_user_pk:str, h_order_pk: str, h_user_pk: str, load_dt:datetime, load_src: str) -> None:
+    def l_order_user_insert(self, l_order_user: L_order_user) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -159,16 +165,16 @@ class DdsRepository:
                     on conflict(hk_order_user_pk) do nothing;
                     """,
                     {
-                    'hk_order_user_pk': hk_order_user_pk,
-                    'h_order_pk': h_order_pk,
-                    'h_user_pk': h_user_pk,
-                    'load_dt': load_dt,
-                    'load_src': load_src
+                        'hk_order_user_pk': l_order_user.hk_order_user_pk,
+                        'h_user_pk': l_order_user.h_user_pk,
+                        'h_order_pk': l_order_user.h_order_pk,
+                        'load_dt': l_order_user.load_dt,
+                        'load_src': l_order_user.load_src
                     }
                 )
 
     #сателлиты
-    def s_user_names_insert(self, h_user_pk:str, username: str, userlogin: str, load_dt:datetime, load_src: str, hk_user_names_hashdiff: str) -> None:
+    def s_user_names_insert(self, user_names: S_user_names) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -178,35 +184,36 @@ class DdsRepository:
                     on conflict(hk_user_names_hashdiff) do nothing;
                     """,
                     {
-                    'h_user_pk': h_user_pk,
-                    'username': username,
-                    'userlogin': userlogin,
-                    'load_dt': load_dt,
-                    'load_src': load_src,
-                    'hk_user_names_hashdiff': hk_user_names_hashdiff
+                        'h_user_pk': user_names.h_user_pk,
+                        'username': user_names.username,
+                        'userlogin': user_names.userlogin,
+                        'load_dt': user_names.load_dt,
+                        'load_src': user_names.load_src,
+                        'hk_user_names_hashdiff': user_names.hk_user_names_hashdiff
                     }
                 )
 
 
-    def s_product_names_insert(self, h_product_pk:str, name: str, load_dt:datetime, load_src: str, hk_product_names_hashdiff: str) -> None:
+    def s_product_names_insert(self,objs: List[S_product_names]) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
-                cur.execute(
+                for obj in objs:
+                    cur.execute(
                     """
                     insert into dds.s_product_names (h_product_pk, name, load_dt, load_src, hk_product_names_hashdiff)
                     values (%(h_product_pk)s, %(name)s, %(load_dt)s, %(load_src)s, %(hk_product_names_hashdiff)s)
                     on conflict(hk_product_names_hashdiff) do nothing;
                     """,
                     {
-                    'h_product_pk': h_product_pk,
-                    'name': name,
-                    'load_dt': load_dt,
-                    'load_src': load_src,
-                    'hk_product_names_hashdiff':  hk_product_names_hashdiff
-                    }
-                )
+                            'h_product_pk': obj.h_product_pk,
+                            'name': obj.name,
+                            'load_dt': obj.load_dt,
+                            'load_src': obj.load_src,
+                            'hk_product_names_hashdiff': obj.hk_product_names_hashdiff
+                        }
+                    )
 
-    def s_restaurant_names_insert(self, h_restaurant_pk:str, name: str, load_dt:datetime, load_src: str, hk_restaurant_names_hashdiff: str) -> None:
+    def s_restaurant_names_insert(self, restaurant_names: S_restaurant_names) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -216,15 +223,15 @@ class DdsRepository:
                     on conflict(hk_restaurant_names_hashdiff) do nothing;
                     """,
                     {
-                    'h_restaurant_pk': h_restaurant_pk,
-                    'name': name,
-                    'load_dt': load_dt,
-                    'load_src': load_src,
-                    'hk_restaurant_names_hashdiff':  hk_restaurant_names_hashdiff
+                        'h_restaurant_pk': restaurant_names.h_restaurant_pk,
+                        'name': restaurant_names.name,
+                        'load_dt': restaurant_names.load_dt,
+                        'load_src': restaurant_names.load_src,
+                        'hk_restaurant_names_hashdiff': restaurant_names.hk_restaurant_names_hashdiff
                     }
                 )
 
-    def s_order_cost_insert(self, h_order_pk:str, cost: float, payment: float, load_dt:datetime, load_src: str, hk_order_cost_hashdiff: str) -> None:
+    def s_order_cost_insert(self, order_cost: S_order_cost) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -234,16 +241,16 @@ class DdsRepository:
                     on conflict(hk_order_cost_hashdiff) do nothing;
                     """,
                     {
-                    'h_order_pk': h_order_pk,
-                    'cost': cost,
-                    'payment': payment,
-                    'load_dt': load_dt,
-                    'load_src': load_src,
-                    'hk_order_cost_hashdiff':  hk_order_cost_hashdiff
+                        'h_order_pk': order_cost.h_order_pk,
+                        'cost': order_cost.cost,
+                        'payment': order_cost.payment,
+                        'load_dt': order_cost.load_dt,
+                        'load_src': order_cost.load_src,
+                        'hk_order_cost_hashdiff': order_cost.hk_order_cost_hashdiff
                     }
                 )
 
-    def s_order_status_insert(self, h_order_pk:str, status: str, load_dt:datetime, load_src: str, hk_order_status_hashdiff: str) -> None:
+    def s_order_status_insert(self, order_status: S_order_status) -> None:
         with self._db.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
@@ -253,10 +260,10 @@ class DdsRepository:
                     on conflict(hk_order_status_hashdiff) do nothing;
                     """,
                     {
-                    'h_order_pk': h_order_pk,
-                    'status': status,
-                    'load_dt': load_dt,
-                    'load_src': load_src,
-                    'hk_order_status_hashdiff':  hk_order_status_hashdiff
+                        'h_order_pk': order_status.h_order_pk,
+                        'status': order_status.status,
+                        'load_dt': order_status.load_dt,
+                        'load_src': order_status.load_src,
+                        'hk_order_status_hashdiff': order_status.hk_order_status_hashdiff
                     }
                 )
